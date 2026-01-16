0d92d0250f2e1d2e3bfc98aded4c140e80673988
Muhammad Touhidul Islam <13749639+touhidion@users.noreply.github.com>
2026-01-16 14:44:35 +0600

    Add files via upload

---
+++ src/main/java/com/helloheads/helloheadsapi/HelloheadsApiApplication.java
@@ -0,0 +1,13 @@
+package com.helloheads.helloheadsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloheadsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloheadsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helloheads/helloheadsapi/HelloheadsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloheads.helloheadsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloheadsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
